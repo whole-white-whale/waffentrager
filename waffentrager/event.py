8dd7f575d77f221b5ca6b5de92fbd418d759be1f
Sergey Kirichenko <whole-white-whale@yandex.ru>
2024-10-02 18:45:09 +0300

    Create a task model

---
+++ waffentrager/event.py
@@ -0,0 +1,21 @@
+import datetime
+
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Event(BaseModel):
+    name: str = Field(description="A short and descriptive name of the event")
+
+    description: Optional[str] = Field(
+        default=None, description="A full and detailed description of the event"
+    )
+
+    date: datetime.date = Field(description="A date of the event")
+
+    time: Optional[datetime.time] = Field(
+        default=None, description="A time of the event"
+    )
+
+    location: Optional[str] = Field(default=None, description="A location of the event")
